930a9862dca1fbc1c46b75046081e3ae8fb0a114
Marcos Á. Medina <marcmedi1@hotmail.com>
2020-10-03 21:12:12 -0300

    Header component removed + 2 new component

--- spacedream/src/App.js
+++ spacedream/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./client";
-import Header from './components/Header'
 import Footer from "./components/Footer";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 import Welcome from "./components/Welcome";
+import MenuBar from "./components/MenuBar"
+import TabMenu from "./components/TabMenu"
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 
@@ -19,8 +19,9 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <Route path="/" component={Header} />
+        <Route path="/" component={MenuBar} />
         <Route path="/" component={Welcome} />
+        <Route path="/" component={TabMenu} />
         <Switch>
           <Route path="/login" component={LoginForm} />
           <Route path="/register" component={RegisterForm} />
